2b1cc127b718d6dd90ff314bb804006c22999eaa
RasmusGyllenhammar <rasmus.gyllenhammar@elev.ga.ntig.se>
2019-09-12 10:55:21 +0200

    läst in användarens namn i konsolen

--- hemläxor/v.37/hemläxor/basics.java
+++ hemläxor/v.37/hemläxor/basics.java
@@ -5,7 +5,11 @@ public class basics {
 		public static void main(String[] args) {
 	
 			 Scanner input = new Scanner(System.in);
-		       
+			 System.out.println("HEllO, What is your name");
+			String name = input.nextLine();
+	
+			
+			 
 
 			
 			
