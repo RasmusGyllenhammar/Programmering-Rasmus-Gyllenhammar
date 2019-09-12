a942bc8192ca61a1cfe71b5dc24765183de8f56b
RasmusGyllenhammar <rasmus.gyllenhammar@elev.ga.ntig.se>
2019-09-12 14:57:34 +0200

    skrivit från adress till telefonnr

--- hemläxor/v.37/hemläxor/basics.java
+++ hemläxor/v.37/hemläxor/basics.java
@@ -5,13 +5,24 @@ public class basics {
 		public static void main(String[] args) {
 	
 			 Scanner input = new Scanner(System.in);
+			
 			 System.out.println("Hej, vad heter du?");
-			String name = input.nextLine();
+			 String name = input.nextLine();
+			
 			System.out.println("hur gammal är du?");
 			String age = input.nextLine();
 			
+			System.out.println("vad är din adress?");
+			String adress = input.nextLine();
 			
+			System.out.println("vad är ditt postnummer?");
+			String postnr = input.nextLine();
 			
+			System.out.println("vilken stad bor du i?");
+			String bostad = input.nextLine();
+			
+			System.out.println("Vad är ditt telefonnummer?");
+			String nr = input.nextLine();
 			 
 
 			
