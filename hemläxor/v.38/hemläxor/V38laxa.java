92135515d1c5767706c9657ba7a62a8355923eca
RasmusGyllenhammar <rasmus.gyllenhammar@elev.ga.ntig.se>
2019-09-19 10:51:57 +0200

    gjort nästan klart högsta och lägsta talen

--- hemläxor/v.38/hemläxor/V38laxa.java
+++ hemläxor/v.38/hemläxor/V38laxa.java
@@ -11,17 +11,27 @@ public class V38laxa {
 		System.out.println("skriv ut 10 tal");
 	    
 		int total=0;
+		int lägsta=0;
+		int högsta=0;
+				
 		for(int i= 0;i<10;i++) { 
 			int nr= input.nextInt();
 			total+=nr;
+			if (lägsta>nr) {
+				lägsta=nr;
+			}
+			if(högsta<nr) {
+				högsta=nr;
+			}
 		}
 		
+		
 		System.out.println("summan:" + total);
-		System.out.println( "medelvärdet:" + total/10);
+		System.out.println( "medelvärdet:" + total/10.);
 		
-		System.out.println("lägsta talet:"  );
+		System.out.println("lägsta talet:" + lägsta );
 		
-		System.out.println("högsta talet:"  );
+		System.out.println("högsta talet:" + högsta);
 		
 
 }}
