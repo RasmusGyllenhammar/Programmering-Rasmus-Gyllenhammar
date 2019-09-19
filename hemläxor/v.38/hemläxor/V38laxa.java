bbb487018064559a3f181bf583901ce3c49bb446
RasmusGyllenhammar <rasmus.gyllenhammar@elev.ga.ntig.se>
2019-09-19 10:41:45 +0200

    skapat scanner, for loop för räkna ut 10 tal

---
+++ hemläxor/v.38/hemläxor/V38laxa.java
@@ -0,0 +1,22 @@
+package hemläxor;
+
+import java.util.Scanner;
+
+public class V38laxa {
+
+	public static void main(String[] args) {
+		
+		Scanner input = new Scanner(System.in);
+
+		System.out.println("skriv ut 10 tal");
+	    
+		int total=0;
+		for(int i= 0;i<10;i++) { 
+			int nr= input.nextInt();
+			total+=nr;
+		}
+		
+		
+		
+
+}}
