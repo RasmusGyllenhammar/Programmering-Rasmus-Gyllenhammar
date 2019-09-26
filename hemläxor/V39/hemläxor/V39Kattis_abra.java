efcfb9c0910b7416792705694663a2e549ab332e
RasmusGyllenhammar <rasmus.gyllenhammar@elev.ga.ntig.se>
2019-09-26 14:22:12 +0200

    skrivit en variabel ska börja med loop

---
+++ hemläxor/V39/hemläxor/V39Kattis_abra.java
@@ -0,0 +1,18 @@
+package hemläxor;
+
+import java.util.Scanner;
+
+public class V39Kattis_abra {
+
+	public static void main(String[] args) {
+	
+	 Scanner input = new Scanner(System.in);
+	 
+	 int x = input.nextInt();
+	
+	 for(int i = 1;i <= x;i++){
+	 
+	  System.out.println(i + "abracadabra");
+	}
+	}
+}
