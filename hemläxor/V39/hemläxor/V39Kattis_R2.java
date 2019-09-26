ad40e10be3276dba58fa641030085a6dfad3f6b3
RasmusGyllenhammar <rasmus.gyllenhammar@elev.ga.ntig.se>
2019-09-26 14:34:35 +0200

    skapat variabler

---
+++ hemläxor/V39/hemläxor/V39Kattis_R2.java
@@ -0,0 +1,21 @@
+package hemläxor;
+
+import java.util.Scanner;
+
+public class V39Kattis_R2 {
+	public static void main(String[] args) {
+		
+		Scanner input = new Scanner(System.in);
+		System.out.println("skriv två nummer");
+		
+		int S = input.nextInt();
+		int R1 = input.nextInt();
+		int r2 = input.nextInt();
+		
+		
+		
+		
+		
+	}
+
+}
