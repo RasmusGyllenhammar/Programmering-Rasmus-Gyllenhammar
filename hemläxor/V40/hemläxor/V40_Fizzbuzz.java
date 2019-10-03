cb94870388119016c26b5c440c348461c55e3049
RasmusGyllenhammar <rasmus.gyllenhammar@elev.ga.ntig.se>
2019-10-03 10:15:58 +0200

    gjort så skriver ut siffor om ej jämnt delbart + ändrade ordningen så de fungera. klar

--- hemläxor/V40/hemläxor/V40_Fizzbuzz.java
+++ hemläxor/V40/hemläxor/V40_Fizzbuzz.java
@@ -5,17 +5,27 @@ import java.util.Scanner;
 public class V40_Fizzbuzz {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
 		Scanner input = new Scanner(System.in);
 		
 		int x= input.nextInt();
 		int y= input.nextInt();
 		int n= input.nextInt();
+		
 	    
-		for(int i=1; i<n; i++);{
-			if(någoting%x==0) {
-				System.out.println("fizz");
+		for(int i = 1; i <= n; i++){
+			
+			if( i % x ==0 && i % y==0) {
+				System.out.println("fizzbuzz");
+			}
+			else if( i % y==0) {
+			System.out.println("buzz");
+			}
+			else if( i % x==0) {
+			System.out.println("fizz");
+				}
+			else {
+			System.out.println(i);
 			}
 		}
 	
