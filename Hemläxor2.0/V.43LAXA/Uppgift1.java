6ff3d45bd735771a476e6a7da0c36afec50bdb8a
RasmusGyllenhammar <rasmus.gyllenhammar@elev.ga.ntig.se>
2019-10-24 11:59:30 +0200

    skapat metoden,  skapat input för radien, anropar volym, skriver ut svaret

---
+++ Hemläxor2.0/V.43LAXA/Uppgift1.java
@@ -0,0 +1,26 @@
+import java.math.*;
+import java.util.Scanner;
+
+public class Uppgift1 {
+
+	public static void main(String[] args) {
+	Scanner input = new Scanner(System.in);
+		
+		//kan skriva in värdet på radien
+		double radius = input.nextDouble();
+		//anropar 
+		volume(radius);
+		
+		//Skriver ut svaret om volymen
+		System.out.println(volume(radius));
+		
+
+	} // har double för vi ska returna ett värde och inte void då
+	public static double volume(double radius) {
+		
+		
+		
+		
+		
+	}
+}
