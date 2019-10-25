1dc14224436ca60afd743472afe399b8e432eaab
RasmusGyllenhammar <rasmus.gyllenhammar@elev.ga.ntig.se>
2019-10-25 14:26:25 +0200

    skapat en metod som skriver en string baklänges

--- Hemläxor2.0/V.43LAXA/Uppgift1.java
+++ Hemläxor2.0/V.43LAXA/Uppgift1.java
@@ -6,16 +6,24 @@ public class Uppgift1 {
 	public static void main(String[] args) {
 	Scanner input = new Scanner(System.in);
 		
-		//kan skriva in värdet på radien
-		double radius = input.nextDouble();
+
 		//anropar 
-		volume(radius);
+		//volume(input.nextDouble());
 		
 		//Skriver ut svaret om volymen
-		System.out.println(volume(radius));
+	//	System.out.println(volume(radius));
+		
+		reverse(input.nextLine());
 		
 
-	} // har double för vi ska returna ett värde och inte void då
+	}
+	
+	/**
+	 *  Metoden räknar ut volym på klotet.
+	 * 
+	 * @param radius   radien för klotet
+	 * @return skickar volym
+	 */
 	public static double volume(double radius) {
 		
 		
@@ -23,4 +31,25 @@ public class Uppgift1 {
 		
 		
 	}
+	/**tar in string, och en tom string som kallas för result. I loopen säger vi att int i = antal bokstäver -1 för vi går från 
+	 * 0-5 och inte 1-6, därför ska i var i>=0 sedan tar vi minus 1 för vi har inte fler bokstäver. Sedan visar vi resulat stringen 
+	 * med str.charat(i) då den e tom från början får det första tecknet. 
+	 * 
+	 * @param str är där vi skriver in namnet
+	 * @return skickar tillbaka namnet fast omvänt
+	 */
+	public static String reverse(String str) {
+		String result = "";
+		for(int i = str.length() - 1; i >= 0; i--) {
+			 result = result + str.charAt(i);
+			 
+		}
+		
+		
+	 	System.out.println(result);
+		return str; 
+	}
+	
+	
+	
 }
