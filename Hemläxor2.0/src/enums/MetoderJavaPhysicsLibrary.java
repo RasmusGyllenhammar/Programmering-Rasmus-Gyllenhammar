2bae534a3ac002d61c66a2c5838b7b283e00dd9e
RasmusGyllenhammar <rasmus.gyllenhammar@elev.ga.ntig.se>
2019-11-11 10:44:28 +0100

    klar med 4, gjort så man kan kalla på vilken vätska i enumens och kan få svar på trycket vid det speciella djupet

--- Hemläxor2.0/src/enums/MetoderJavaPhysicsLibrary.java
+++ Hemläxor2.0/src/enums/MetoderJavaPhysicsLibrary.java
@@ -1,13 +1,18 @@
+package enums;
 import java.util.Scanner;
 
 public class MetoderJavaPhysicsLibrary {
 
+	static double g = 9.82;
+	
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
 		//anropar på metoden, värdet 50
 		fahrenheitToCelsius(50);
 		
 		kelvinToCelsius(0);
+		
+		fluidPressure(FluidTable.WATER, 10);
 	}
 
 	/**
@@ -30,4 +35,17 @@ public class MetoderJavaPhysicsLibrary {
 		System.out.println(celsius);
 		return celsius;
 	}
+	/**
+	 * Räknar ut vätsketrycket i en vätska vid ett visst djup
+	 * @param fluid Denna är till vilken vätska vi väljer
+	 * @param deep Detta är djupet/höjden till vätskan
+	 * @return returnar trycket i vätskan vid det utvalda djupet
+	 */
+	public static double fluidPressure(FluidTable fluid, double deep) {
+		double pressure = g * fluid.density * deep;
+		System.out.println(pressure);
+		return pressure;
+	}
+	
+	public static double 
 }
