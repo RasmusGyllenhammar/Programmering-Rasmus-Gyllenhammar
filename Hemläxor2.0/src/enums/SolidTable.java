add0259857f946c5efea7c3f7703c5d303314ead
RasmusGyllenhammar <rasmus.gyllenhammar@elev.ga.ntig.se>
2019-11-18 10:54:21 +0100

    Lagt till två till på vätske och material tabllen så de blir fem där

--- Hemläxor2.0/src/enums/SolidTable.java
+++ Hemläxor2.0/src/enums/SolidTable.java
@@ -3,7 +3,10 @@ package enums;
 public enum SolidTable {
 
 	ICE(0.92, 2.2, 0, 334),
-	IRON(7.87, 0.45, 1538, 276)
+	IRON(7.87, 0.45, 1538, 276),
+	Titan(4.51, 0.52, 1668, 390),
+	Nickel(8.90, 0.44, 1455, 293),
+	Uran(19.1, 0.12, 1135, 59)
 	;
 
 	double density;
