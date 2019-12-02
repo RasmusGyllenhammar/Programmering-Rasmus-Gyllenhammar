e190531be55828fcf90aeea1326099f574c6c2f1
RasmusGyllenhammar <rasmus.gyllenhammar@elev.ga.ntig.se>
2019-12-02 15:27:06 +0100

    länder

--- Vecka.49/Länder.java
+++ Vecka.49/Länder.java
@@ -1,5 +1,28 @@
+import java.util.Scanner;
+
 public class Länder {
     public static void main(String[] args) {
-        String[] = {"Iceland", "Sweden", "Norway", "Denmark"};
+        Scanner input = new Scanner(System.in);
+
+        String[] countries = {"Iceland", "Sweden", "Norway", "Denmark", "Finland"};
+
+        int points = 0;
+        for(int i = 0; i < 5; i++){
+           String country = input.nextLine();
+           for( String nation : countries){
+               if(nation.equals(country)){
+                   points++;
+               }
+
+
+            }
+
+
+        }
+        System.out.println("Total : " + points);
+
+
+        //Gör en kod som ber användaren att lista alla länder i norden och sen kolla om personen har fått alla rätt
+
     }
 }
