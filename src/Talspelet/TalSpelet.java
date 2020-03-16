d9eb736fba04f601986d88e1815a71c8778668c9
RasmusGyllenhammar <rasmus.gyllenhammar@elev.ga.ntig.se>
2020-03-16 18:22:29 +0100

    La till på checkIfGuessIsRight att om gissningarna överstiger maximala antalet så förlorar man och gör att man kommer till nästa metod som frågar om man vill köra igen eller inte med switch statement

--- src/Talspelet/TalSpelet.java
+++ src/Talspelet/TalSpelet.java
@@ -7,12 +7,15 @@ public class TalSpelet {
    protected static int limitedAmountOfGuesses;
 
    public static void main(String[] args) {
-
-    chooseDifficulty();
+       System.out.println("test");
+       chooseDifficulty();
     checkIfGuessIsRight();
 
     }
 
+    /**
+     *
+     */
     public static void chooseDifficulty(){
         System.out.println("Welcome summoner, select a difficulty between 1(easy), 2(medium), 3(hard)");
 
@@ -40,7 +43,7 @@ public class TalSpelet {
     public static void checkIfGuessIsRight(){
         System.out.println("Now try to guess the number between 1 - 100");
         boolean win = false;
-
+        //test
         int guessCounter = 0;
         int correctNumber = (int)(100 * Math.random());
 
@@ -51,13 +54,30 @@ public class TalSpelet {
             if(guess == correctNumber && guessCounter < limitedAmountOfGuesses) {
                 win = true;
                 System.out.println("WOW YOU WON");
+                rematchOrNot();
             } else if(guess < correctNumber) {
                 System.out.println("Seems like your guess is too low, try a greater number");
             } else if(guess > correctNumber) {
                 System.out.println("Seems like your guess is too high, try a lower number");
             }
+            if(guessCounter >= limitedAmountOfGuesses){
+                System.out.println("You are out of guesses, you lost");
+                rematchOrNot();
+            }
         }
     }
+    public static void rematchOrNot() {
+        System.out.println("Do you want to play again? If so, press 1. If not press 2");
+        int playAgain = input.nextInt();
 
+        switch (playAgain) {
+            case 1:
+                chooseDifficulty();
+                checkIfGuessIsRight();
+
+            case 2:
+                System.out.println("Okay, dont play then :(");
+        }
+    }
 
 }
