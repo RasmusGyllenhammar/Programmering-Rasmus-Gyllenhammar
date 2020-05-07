896ae9b0b5f939b0498e499e38641fcb91607a19
RasmusGyllenhammar <rasmus.gyllenhammar@elev.ga.ntig.se>
2020-05-07 10:48:39 +0200

    Lagt till switch casen för multiplayer och difficulty, har även gjort att jag slumpar ett ord och gör om den till '_'

--- src/HangmanGame/HangmanTheGame.java
+++ src/HangmanGame/HangmanTheGame.java
@@ -11,27 +11,69 @@ public class HangmanTheGame {
     protected static int GuessCounter = 0;
 
     public static void main(String[] args) {
+        greeting();
+
+        chooseDifficulty();
+        randomizeTheWord();
+        resetWordProgress();
 
     }
 
     public static void greeting(){
-        System.out.println("hello, and welcome to the game Hangman! You will have to guess the right word; letter by letter! You can only guess wrong 7 times and after that you will lose");
+        System.out.println("hello, and welcome to the game Hangman! You will have to guess the right word; letter by letter! You can only guess wrong 7 times and after that you will lose \n " + "You could also play with a friend, where one of you choose a word from a list and the other try to guess it");
     }
 
     public static void playMultiplayer(){
+
+        System.out.println("Would you like to play with a friend? if so press 1 or press 2 to play ALONE");
+
+      int playWithFriend = _input.nextInt();
+        switch (playWithFriend){
+            case 1:
+                System.out.println("Ah, play multiplayer");
+
+
+            case 2:
+                System.out.println("OK, play alone");
+        }
 
     }
 
     public static void chooseDifficulty(){
 
+        System.out.println("what difficulty would you like to play? Press 1(complex words), or 2(common words)");
+
+       int difficulty = _input.nextInt();
+
+        switch (difficulty){
+            case 1:
+                System.out.println("ah, ok you want more complex words huh? Good luck");
+                unknownWordsToGuessOn = new String[]{"southkorea", "earthflax", "nti"};
+                break;
+
+            case 2:
+                System.out.println("ah, ok you want more COMMON WORDS, GOOD LUCK");
+                unknownWordsToGuessOn = new String[]{"goodmorning", "hello", "kebab"};
+                break;
+        }
+
     }
 
     public static char[] randomizeTheWord(){
-
-        return randomizeTheWord();
+           char[] makeRandomWord = unknownWordsToGuessOn[randomize.nextInt(unknownWordsToGuessOn.length)].toCharArray();
+        System.out.println(makeRandomWord); // kollar det slumpade ordet innan det görs om till understräck
+           return makeRandomWord;
     }
 
     public static void resetWordProgress(){
+
+        char[] wordReplacedByUnderScore = new char[randomizeTheWord().length];
+
+        for(int i = 0; i < wordReplacedByUnderScore.length; i++){
+          wordReplacedByUnderScore[i] = '_';
+        }
+
+        System.out.println(wordReplacedByUnderScore); //Skriver ut det slumpade ordet i en 
 
     }
 
@@ -48,9 +90,7 @@ public class HangmanTheGame {
 
     }
 
-    public static void game(){
 
-    }
 
     public static boolean hasWon(char[] win){
 
@@ -58,6 +98,11 @@ public class HangmanTheGame {
     }
 
     public static void printHangmanPhase(){
+
+
+    }
+
+    public static void game(){
 
     }
 }
