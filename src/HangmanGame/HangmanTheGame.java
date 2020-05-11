798470ea4c64de0c9408a0901c2218563cb06f6d
RasmusGyllenhammar <rasmus.gyllenhammar@elev.ga.ntig.se>
2020-05-11 10:52:45 +0200

    har gjort hangman faserna och gjort metoden uppdateWordProgress med en for loop som kollar att ifall man gissat rätt, plus att gjort rematchOrNot också med switch casen,  gjorde en ny global variable för att kunna sätta ordet i en ny så jag inte behöver kalla på metoden vilket blir fel då den upprepar och återställs

--- src/HangmanGame/HangmanTheGame.java
+++ src/HangmanGame/HangmanTheGame.java
@@ -1,5 +1,6 @@
 package HangmanGame;
 
+import java.util.ArrayList;
 import java.util.Random;
 import java.util.Scanner;
 
@@ -8,15 +9,20 @@ public class HangmanTheGame {
     protected static Random randomize = new Random();
     protected static String[] unknownWordsToGuessOn = {};
     protected static int amountOfGuesses = 7;
-    protected static int GuessCounter = 0;
+    protected static int guessCounter = 0;
+    protected static String makeRandomWord;
+    protected static char[] wordProgress;
+
 
     public static void main(String[] args) {
         greeting();
 
         chooseDifficulty();
         randomizeTheWord();
-        resetWordProgress();
+       wordProgress = resetWordProgress();
+        updateWordProgress();
 
+     
     }
 
     public static void greeting(){
@@ -44,11 +50,12 @@ public class HangmanTheGame {
         System.out.println("what difficulty would you like to play? Press 1(complex words), or 2(common words)");
 
        int difficulty = _input.nextInt();
+       _input.nextLine(); //läser in nästa rad, tror siffran är min input
 
         switch (difficulty){
             case 1:
                 System.out.println("ah, ok you want more complex words huh? Good luck");
-                unknownWordsToGuessOn = new String[]{"southkorea", "earthflax", "nti"};
+                unknownWordsToGuessOn = new String[]{"southkorea", "earthflaxxxxxxxxxxxxxxxxxxxxxxxxxx", "nti"};
                 break;
 
             case 2:
@@ -59,31 +66,61 @@ public class HangmanTheGame {
 
     }
 
-    public static char[] randomizeTheWord(){
-           char[] makeRandomWord = unknownWordsToGuessOn[randomize.nextInt(unknownWordsToGuessOn.length)].toCharArray();
-        System.out.println(makeRandomWord); // kollar det slumpade ordet innan det görs om till understräck
-           return makeRandomWord;
+    public static void randomizeTheWord(){
+           makeRandomWord = unknownWordsToGuessOn[randomize.nextInt(unknownWordsToGuessOn.length)];
+
+
     }
 
-    public static void resetWordProgress(){
+    public static char[] resetWordProgress(){
 
-        char[] wordReplacedByUnderScore = new char[randomizeTheWord().length];
+      char[] wordReplacedByUnderScore = new char[makeRandomWord.length()];
 
         for(int i = 0; i < wordReplacedByUnderScore.length; i++){
           wordReplacedByUnderScore[i] = '_';
         }
-
-        System.out.println(wordReplacedByUnderScore); //Skriver ut det slumpade ordet i en 
+      System.out.println(makeRandomWord);; // kollar det slumpade ordet innan det görs om till understräck
+       System.out.println(String.copyValueOf(wordReplacedByUnderScore));; //Skriver ut det slumpade ordet i "_"
+        return  wordReplacedByUnderScore;
 
     }
 
-    public static void rematchOrNot(int playAgain){
-        playAgain = _input.nextInt();
+    public static void rematchOrNot(){
+       int playAgain = _input.nextInt();
+
+       switch (playAgain){
+           case 1:
+               playMultiplayer();
+               break;
+
+           case 2:
+               System.out.println("dont play then, goodbye");
+               System.exit(0);
+               break;
+       }
 
     }
 
     public static void updateWordProgress(){
+        char playerGuess = _input.nextLine().toLowerCase().charAt(0);
+        System.out.println(playerGuess);
 
+       for(int i = 0; i < wordProgress.length; i++){
+           if(makeRandomWord.charAt(i) == playerGuess){
+               wordProgress[i] = playerGuess; //återställde innan nör jag kallade på resetwordpgroess
+               System.out.println("hello");
+           }
+           System.out.println(wordProgress[i]);
+
+       }
+
+
+
+    }
+
+    public static char errorSearchFromInput(){
+
+        return 'd';
     }
 
     public static void wrongGuess(){
@@ -99,6 +136,71 @@ public class HangmanTheGame {
 
     public static void printHangmanPhase(){
 
+        switch (guessCounter){
+            case 1:
+                System.out.println("['''\\n\" +\n" +
+                        "                            \"  +---+\\n\" +\n" +
+                        "                            \"  |   |\\n\" +\n" +
+                        "                            \"      |\\n\" +\n" +
+                        "                            \"      |\\n\" +\n" +
+                        "                            \"      |\\n\" +\n" +
+                        "                            \"      |\\n\" +\n" +
+                        "                            \"=========''', '''");
+                break;
+            case 2:
+                System.out.println("  +---+\\n\" +\n" +
+                        "                            \"  |   |\\n\" +\n" +
+                        "                            \"  O   |\\n\" +\n" +
+                        "                            \"      |\\n\" +\n" +
+                        "                            \"      |\\n\" +\n" +
+                        "                            \"      |\\n\" +\n" +
+                        "                            \"=========''', '''");
+                break;
+            case 3:
+                System.out.println("  +---+\\n\" +\n" +
+                        "                            \"  |   |\\n\" +\n" +
+                        "                            \"  O   |\\n\" +\n" +
+                        "                            \"  |   |\\n\" +\n" +
+                        "                            \"      |\\n\" +\n" +
+                        "                            \"      |\\n\" +\n" +
+                        "                            \"=========''', '''");
+                break;
+            case 4:
+                System.out.println("  +---+\\n\" +\n" +
+                        "                            \"  |   |\\n\" +\n" +
+                        "                            \"  O   |\\n\" +\n" +
+                        "                            \" /|   |\\n\" +\n" +
+                        "                            \"      |\\n\" +\n" +
+                        "                            \"      |\\n\" +\n" +
+                        "                            \"=========''', '''");
+                break;
+            case 5:
+                System.out.println("  +---+\\n\" +\n" +
+                        "                            \"  |   |\\n\" +\n" +
+                        "                            \"  O   |\\n\" +\n" +
+                        "                            \" /|\\\\  |\\n\" +\n" +
+                        "                            \"      |\\n\" +\n" +
+                        "                            \"      |\\n\" +\n" +
+                        "                            \"=========''', '''");
+                break;
+            case 6:
+                System.out.println(" +---+\\n\" +\n" +
+                        "                            \"  |   |\\n\" +\n" +
+                        "                            \"  O   |\\n\" +\n" +
+                        "                            \" /|\\\\  |\\n\" +\n" +
+                        "                            \" /    |\\n\" +\n" +
+                        "                            \"      |\\n\" +\n" +
+                        "                            \"=========''', '''");
+                break;
+            case 7:
+                System.out.println("  +---+\\n\" +\n" +
+                        "                            \"  |   |\\n\" +\n" +
+                        "                            \"  O   |\\n\" +\n" +
+                        "                            \" /|\\\\  |\\n\" +\n" +
+                        "                            \" / \\\\  |\\n\" +\n" +
+                        "                            \"      |\\n\" +\n" +
+                        "                            \"=========''']");
+        }
 
     }
 
